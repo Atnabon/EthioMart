1e4ab2d6b95b8b9774f1b3cc4911f7637bfb07e6
atnabon <atnabond@gmail.com>
2024-10-01 10:38:45 +0300

    labeled_data_conll

--- scripts/amharictextprocessor.py
+++ scripts/amharictextprocessor.py
@@ -4,19 +4,16 @@ import pandas as pd
 
 class AmharicTextPreprocessor:
     def __init__(self):
-        # Allow Amharic letters, numbers, spaces, and the '/' character
-        self.allowed_characters = re.compile(r'[^ሀ-ፐ0-9\s/]')  # Exclude English letters
-        # self.price_pattern = re.compile(
-        #     r'(መሸጫ\s*(?:[።፡.-]?\s*)?ዋጋ\s*\d+ብር|መሸጫ\s*\d+\s*ብር|መሸጫ\s*ብር\s*\d+|መሸጫ\s*\d+|(ዋጋ|በ)\s*\d+\s*ብር|\d+\s*ብር|(?:\d+ብር))'
-        # )
+        self.allowed_characters = re.compile(r'[^ሀ-ፐ0-9\s/]')  
+       
 
     def normalize_text(self, text):
         if not isinstance(text, str) or not text.strip():
             return np.nan
 
-        # Remove unwanted characters but keep specific patterns intact
-        text = re.sub(self.allowed_characters, '', text)  # Remove unwanted characters
-        text = re.sub(r'\s+', ' ', text).strip()  # Remove extra spaces and newlines
+        
+        text = re.sub(self.allowed_characters, '', text) 
+        text = re.sub(r'\s+', ' ', text).strip()
 
         return text
 
@@ -25,6 +22,5 @@ class AmharicTextPreprocessor:
         return normalized_text if normalized_text and normalized_text != '' else np.nan
 
     def preprocess_dataframe(self, df, text_column):
-        # Apply preprocessing to the specified text column only
         df['preprocessed_message'] = df[text_column].apply(self.preprocess)
         return df
